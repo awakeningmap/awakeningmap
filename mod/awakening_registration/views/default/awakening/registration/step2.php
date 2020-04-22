fcf94bf08b7306f64c9a90371e9aeb437a8178d3
Matt Beckett <beck24@gmail.com>
2020-04-21 22:43:29 -0700

    add back link to registration steps (#8)
    Fixes #3

--- mod/awakening_registration/views/default/awakening/registration/step2.php
+++ mod/awakening_registration/views/default/awakening/registration/step2.php
@@ -6,6 +6,13 @@ $yesnomaybe = <<<YESNOMAYBE
     <button class="elgg-button-action button-dunno">I don't know</button>
 YESNOMAYBE;
 
+echo elgg_view('output/url', [
+    'text' => '<< Back',
+    'href' => 'javascript:void(0)',
+    'class' => 'step step-back',
+    'data-step' => 2
+]);
+
 echo elgg_view_module('info', 'Would you like to meet and share with others who are also awakening?', $yesnomaybe, [
     'class' => 'step step-2'
 ]);
